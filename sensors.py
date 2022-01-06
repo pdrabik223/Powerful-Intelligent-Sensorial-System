4e8804c4b0fc7e2106f7ef23e2643a16dce4254a
Piotr Drabik <57156673+piotr233@users.noreply.github.com>
2022-01-06 11:52:56 +0100

    Temp sensor update

---
+++ sensors.py
@@ -0,0 +1,11 @@
+# sensors for raspberry
+import time
+from w1thermsensor import W1ThermSensor
+
+sensor = W1ThermSensor()
+while True:
+    temperature = sensor.get_temperature()
+    print("The temperature is %s celsius" % temperature)
+    time.sleep(1)
+
+
